a06eab515c427ae18ff6b9acbcc2b58ffc5cae5f
danielavictor <danielavictornotion@gmail.com>
2025-06-02 22:57:47 -0400

    Home, dashboard y demás pantallas con datatable pero sin datos reales

--- sociarepas/home/urls.py
+++ sociarepas/home/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
+from food_filling import views as food_filling_views
+from food_type import views as food_type_views
+from order import views as order_views
 
 app_name = 'home'
 urlpatterns = [
-    path("", views.home, name="home"), 
+    path("", views.home, name="home"),
+    path("dashboard/", views.dashboard, name="dashboard"),
+    path("food_filling/", food_filling_views.food_filling, name="food_filling"),
+    path("food_type/", food_type_views.food_type, name="food_type"),
+    path("order/", order_views.order, name="order"),
 ]
--- sociarepas/sociarepas/urls.py
+++ sociarepas/sociarepas/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 from django.conf import settings
 
 urlpatterns = [
@@ -25,6 +26,7 @@ urlpatterns = [
     path("food_type/", include("food_type.urls")),
     path("home/", include("home.urls")),
     path("login/", include("login.urls")),
+    # path('', lambda request: redirect('login:login')),
     path("order/", include("order.urls")),
 ]
 
