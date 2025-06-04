efe327f52407f892aefdfbf768b2cae293d44529
Victor P <deerpg17x@gmail.com>
2025-06-04 08:24:19 -0400

    Log in Funcional VP falta validacion por urls (dashboard.html, login.html, home.html, url.py de login, views.py de login)

--- sociarepas/login/urls.py
+++ sociarepas/login/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'login'
 urlpatterns = [
-    path("", views.login, name="login"), 
+    path("", views.login, name="login"),
+    path("logout/", views.log_out, name="logout"),
 ]
--- sociarepas/login/views.py
+++ sociarepas/login/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login as auth_login
+from django.http import JsonResponse
+from django.contrib.auth import logout
+from django.shortcuts import redirect
 def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return JsonResponse({'status': 'success', 'message': 'Inicio de sesión exitoso.', 'redirect_url': '/home/dashboard/'})
+        else:
+            return JsonResponse({'status': 'error', 'message': 'Usuario o contraseña incorrectos.'})
     return render(request, 'login.html')
+
+def log_out(request):
+    logout(request)
+    return redirect('/login/')
