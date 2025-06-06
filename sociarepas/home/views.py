c2fffec9932b9db5ecbed88187db385cd5508955
danielavictor <danielavictornotion@gmail.com>
2025-06-06 10:52:46 -0400

    La sidebar ya queda por detrás del backdrop; dashboard funcionando la tabla

--- sociarepas/home/views.py
+++ sociarepas/home/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from food_filling.models import Food_Filling, Supplier
+from food_filling.forms import StockRequestForm
+from django.db.models import Sum
 
 @login_required
 def home(request):
@@ -7,4 +10,9 @@ def home(request):
 
 @login_required
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    food_filling = Food_Filling.objects.annotate(total_quantity=Sum('stock__quantity'))
+    stock_request_form = StockRequestForm()
+    return render(request, 'dashboard.html',{
+        'food_filling': food_filling,
+        'stock_request_form': stock_request_form,
+    })
